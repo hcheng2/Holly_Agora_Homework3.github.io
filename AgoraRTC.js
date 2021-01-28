f64055cd05df864d0bae20a0c9ebaba2aabb68a9
hcheng2 <76693746+hcheng2@users.noreply.github.com>
2021-01-27 20:35:05 -0500

    Update AgoraRTC.js

--- AgoraRTC.js
+++ AgoraRTC.js
@@ -1,40 +1,68 @@
-let handlefail = function(err){
-    console.log(err);
-}
+let handleFail = function(err){
+    console.log("Error: ", err);
+};
 
-function addVideoStream(streamId){
-    let remoteContainer = document.getElementById("remote-stream");
-    let streamDiv = document.createElement("div");
-    streamDiv.id = streamId;
-    streamDiv.style.transform = "rotateY(180deg)";
-  //  streamDiv.style.height = "600px";
-    remoteContainer.appendChild(streamDiv);
+let participants = "";
+let numRemote = 0;
+
+let remoteContainer1 = document.getElementById("remote-stream");
+let remoteContainer2 = document.getElementById("remote-stream2");
+let remoteContainer3 = document.getElementById("remote-stream3");
+
+function addParticipant(name){
+  //  username = document.getElementById("username").value;
+    participants += name;
+    participants += "<br>";
+    document.getElementById("participants").innerHTML = participants;    
 }
+ 
+function addVideoStream(elementId){
+    let streamDiv = document.createElement("div");
+    streamDiv.id = elementId;
+    if (numRemote == 0)
+        remoteContainer1.appendChild(streamDiv);
+    else if (numRemote == 1)
+        remoteContainer2.appendChild(streamDiv);
+    else if (numRemote == 2)
+        remoteContainer3.appendChild(streamDiv);
+    numRemote++;
+};
 
 document.getElementById("join").onclick = function(){
     let channelName = document.getElementById("channel").value;
     let username = document.getElementById("username").value;
     let appID = "2400b34296a64a5295504ebc9e2a741f";
-    
     let client = AgoraRTC.createClient({
         mode: "live",
         codec: "h264"    
-    })
-    client.init(appID,() => console.log("AgoraRTC connected"), handlefail)
-    client.join(
-        null,
-        channelName,
-        username,
-        ()=>{
-            var localStream = AgoraRTC.createStream({
-                video: true,
-                audio: true,
-            })
-            localStream.init(function(){
-                localStream.play("self-stream")
-                console.log(`App id: ${appId}\nChannel id: ${channelName}`)
-                client.publish(localStream);
-            })
-        }
-    )
+    });
+    
+    client.init(appID);
+  
+    username = document.getElementById("username").value;
+    addParticipant(username);
+
+    client.join(null, channelName, username, ()=>{
+        let localStream = AgoraRTC.createStream({
+            audio: true,
+            video: true,
+        });
+        localStream.init(()=>{
+            localStream.play("self-stream");
+            client.publish(localStream, handleFail);
+        }, handleFail);
+    }, handleFail);
+
+    client.on("stream-added", function(evt){
+        client.subscribe(evt.stream, handleFail);
+        let name = evt.stream.getElementById("username").value;
+        console.log(name);
+        addParticipant(name);
+    });
+    client.on("stream-subscribed", function(evt){
+        let stream = evt.stream;
+        let streamId = String(stream.getId());
+        addVideoStream(streamId);
+        stream.play(streamId);
+    });
 }
